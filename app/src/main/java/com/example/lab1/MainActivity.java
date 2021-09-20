ceee3cc6c5123a5f82f536e9a00f905f15f62f78
hollyholiday <mv.makeenko@icloud.com>
2021-09-20 10:52:20 +0300

    message

---
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.lab1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        TextView tv = (TextView)findViewById(R.id.textView);
+        tv.setText("You pressed the button");
+    }
+}
